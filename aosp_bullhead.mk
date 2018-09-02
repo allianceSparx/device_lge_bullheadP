96c05bd0d939d0a210c237eeb5cf09e5d63b7ce0
allianceSparx <alliancesparx@gmail.com>
2018-09-02 17:59:49 +0100

    Disable verity

--- aosp_bullhead.mk
+++ aosp_bullhead.mk
@@ -29,9 +29,9 @@ PRODUCT_DEVICE := bullhead
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on BullHead
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_RESTRICT_VENDOR_FILES := true
+#PRODUCT_RESTRICT_VENDOR_FILES := true
 
-PRODUCT_COPY_FILES += device/lge/bullhead/fstab.aosp_bullhead:root/fstab.bullhead
+#PRODUCT_COPY_FILES += device/lge/bullhead/fstab.aosp_bullhead:root/fstab.bullhead
 
 $(call inherit-product, device/lge/bullhead/device.mk)
 $(call inherit-product-if-exists, vendor/lge/bullhead/device-vendor.mk)
